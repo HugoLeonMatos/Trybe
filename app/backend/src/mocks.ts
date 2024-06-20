292d903d7eb783658f6fff757fd4d6f2dd9ea557
Hugo Leon <hugoleon@outlook.com>
2024-06-20 00:24:53 -0300

    testes pimeiro fluxo

---
+++ app/backend/src/mocks.ts
@@ -0,0 +1,8 @@
+const team = { id: 1, teamName: 'Bahia' };
+
+const teams = [team];
+
+export {
+  team,
+  teams,
+};
--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -7,12 +7,31 @@ import { app } from '../app';
 import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
+import Teams from '../database/models/Teams';
+import { team, teams } from '../mocks'
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
+
+describe('Team test', function () {
+  it('return All Teams', async function () {
+    sinon.stub(Teams, 'findAll').resolves(teams as any);
+    const { status, body } = await chai.request(app).get('/teams');
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(teams)
+  })
+
+  it('return Id  Teams', async function () {
+    sinon.stub(Teams, 'findByPk').resolves(team as any);
+    const { status, body } = await chai.request(app).get('/teams/1');
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(team);
+
+  })
+
+
   /**
    * Exemplo do uso de stubs com tipos
    */
@@ -39,7 +58,8 @@ describe('Seu teste', () => {
   //   expect(...)
   // });
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
+  // it('Seu sub-teste', () => {
+  //   expect(false).to.be.eq(true);
+  // });
+  afterEach(sinon.restore);
 });
