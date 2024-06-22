d63a1d2f6bf898053e4b55d6a062c9948f4e30c1
Hugo Leon <hugoleon@outlook.com>
2024-06-22 16:19:15 -0300

    req 12, estava errando na hora de verificar, tinha que exportar o que ja tinha feito e não reimpotar

--- app/backend/src/middlewares/ValidToken.ts
+++ app/backend/src/middlewares/ValidToken.ts
@@ -1,14 +1,19 @@
 import * as jwt from 'jsonwebtoken';
+
 import { RequestHandler } from 'express';
+import JwtUtils from '../utils/JwtUtils';
 
 const validToken: RequestHandler = async (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) return res.status(401).json({ message: 'Token not found' });
 
+  const extractToken = authorization.split(' ')[1];
   try {
-    const user = jwt.verify(authorization, process.env.JWT_SECRET as string);
-    req.body.user = user;
+    // const user = jwt.verify(extractToken, process.env.JWT_SECRET as string);
+    const user = JwtUtils.verify(extractToken);
+    req.body = user;
+    req.body = { ...req.body, user };
     console.log('passei aqui', user);
     next();
   } catch (error) {
