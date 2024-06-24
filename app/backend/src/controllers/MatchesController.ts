97d053ddabe85ef3a4d2d31eaa4677bc9c4e0cac
Hugo Leon <hugoleon@outlook.com>
2024-06-23 22:24:05 -0300

    req 16

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -5,10 +5,26 @@ import mapStatusHTTP from '../utils/mapStatusHTTP';
 export default class MatchesController {
   constructor(
     private matchesService = new MatchesService(),
-  ) { this.matchesService = matchesService; }
+  ) { }
 
-  public async getAllMatches(_req: Request, res: Response): Promise<Response> {
-    const serviceResponse = await this.matchesService.getAllMatches();
+  public async getAllMatches(req: Request, res: Response): Promise<Response> {
+    const { inProgress } = req.query;
+    const serviceResponse = await this.matchesService.getFilteredMatchesProgress(String(inProgress));
     return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
   }
+
+  // public async getFilteredMatches(_req: Request, res: Response): Promise<Response> {
+  // const inProgressString = _req.query.inProgress;
+  // let inProgress: boolean;
+  // if (typeof inProgressString === 'string') {
+  //   inProgress = inProgressString === 'true';
+  // } else {
+  //   inProgress = false;
+  // }
+  // if (_req.query.inProgress) {
+  //   const serviceResponse = await this.matchesService.getFilterMatchesProgress(_req.query.inProgress === 'true');
+  // }
+  // const serviceResponse = await this.matchesService.getFilterMatchesProgress(inProgress);
+  // return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+  // }
 }
--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -8,21 +8,62 @@ export default class MatchesService {
     private Model = Matches,
   ) { }
 
-  public async getAllMatches(): Promise<ServiceResponse<IMatches[]>> {
-    const allMatches = await this.Model.findAll({
+  // public async getAllMatches(inProgress: string): Promise<ServiceResponse<IMatches[]>> {
+  //   const allMatches = await this.Model.findAll({
+  //     include: [{ model: Teams, as: 'homeTeam', attributes: ['teamName'], },
+  //     { model: Teams, as: 'awayTeam', attributes: ['teamName'], },],
+  //   });
+  //   return { status: 'SUCCESSFUL', data: allMatches };
+  // }
+
+  // public async getFilterMatchesProgress(inProgress: string): Promise<ServiceResponse<IMatches[]>> {
+  //   let matchesFiltered: Matches[];
+
+  //   if (inProgress === 'true') {
+  //     matchesFiltered = await this.Model.findAll({
+  //       include: [
+  //         { model: Teams, as: 'homeTeam', attributes: ['teamName'] },
+  //         { model: Teams, as: 'awayTeam', attributes: ['teamName'] },
+  //       ],
+  //       where: { inProgress: true },
+  //     });
+  //   } else if (inProgress === 'false') {
+  //     matchesFiltered = await this.Model.findAll({
+  //       include: [
+  //         { model: Teams, as: 'homeTeam', attributes: ['teamName'] },
+  //         { model: Teams, as: 'awayTeam', attributes: ['teamName'] },
+  //       ],
+  //       where: { inProgress: false },
+  //     });
+  //   } else {
+  //     matchesFiltered = await this.Model.findAll({
+  //       include: [
+  //         { model: Teams, as: 'homeTeam', attributes: ['teamName'] },
+  //         { model: Teams, as: 'awayTeam', attributes: ['teamName'] },
+  //       ],
+  //     });
+  //   }
+  //   return { status: 'SUCCESSFUL', data: matchesFiltered };
+  // }
+
+  public async getFilteredMatchesProgress(inProgress: string)
+    : Promise<ServiceResponse<IMatches[]>> {
+    const whereClause: { inProgress?: boolean } = {};
+
+    if (inProgress === 'true') {
+      whereClause.inProgress = true;
+    } else if (inProgress === 'false') {
+      whereClause.inProgress = false;
+    }
+
+    const matchesFiltered = await this.Model.findAll({
       include: [
-        {
-          model: Teams,
-          as: 'homeTeam',
-          attributes: ['teamName'],
-        },
-        {
-          model: Teams,
-          as: 'awayTeam',
-          attributes: ['teamName'],
-        },
+        { model: Teams, as: 'homeTeam', attributes: ['teamName'] },
+        { model: Teams, as: 'awayTeam', attributes: ['teamName'] },
       ],
+      where: whereClause,
     });
-    return { status: 'SUCCESSFUL', data: allMatches };
+
+    return { status: 'SUCCESSFUL', data: matchesFiltered };
   }
 }
